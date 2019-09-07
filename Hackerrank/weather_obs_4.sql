7b938417958097c2ab881b11c87d61f49d24ee9b
Mhirley Lopes <mhiloca@gmail.com>
2019-09-07 20:09:11 -0300

    uncommented code

--- Hackerrank/weather_obs_4.sql
+++ Hackerrank/weather_obs_4.sql
@@ -1,15 +1,15 @@
---  SELECT title,
---         Char_length(title)
---  FROM   books
---  WHERE  Char_length(title) = (SELECT Min(Char_length(title))
---                              FROM   books
---                              ORDER  BY title
---                              LIMIT  1)
---          OR Char_length(title) = (SELECT Max(Char_length(title))
---                                  FROM   books
---                                  ORDER  BY title DESC
---                                  LIMIT  1)
---  ORDER  BY ( Char_length(title) );
+SELECT title,
+  Char_length(title)
+FROM   books
+WHERE  Char_length(title) = (SELECT Min(Char_length(title))
+                       FROM   books
+                       ORDER  BY title
+                       LIMIT  1)
+   OR Char_length(title) = (SELECT Max(Char_length(title))
+                           FROM   books
+                           ORDER  BY title DESC
+                           LIMIT  1)
+ORDER  BY ( Char_length(title) );
 
 SELECT city,
        Char_length(city)
